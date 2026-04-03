eec4f8cd592e40b98743e73eaee507d45f857674
Will Lehman <william.p.lehman@usace.army.mil>
2026-04-03 13:08:01 +0000

    fixes #85

--- src/app-components/manage-survey.jsx
+++ src/app-components/manage-survey.jsx
@@ -1,123 +1,3 @@
-/*import { Button, Checkboxes } from "@usace/groundwork";
-import { useConnect } from "redux-bundler-hook";
-import MemberTable from "./member-table";
-export default function ManageSurvey() {
-  const {
-    survey,
-    doUpdateSurvey,
-    doUpdateDashboardView,
-    doCompleteSurvey,
-    doRestartSurvey,
-  } = useConnect(
-    "selectSurvey",
-    "doUpdateSurvey",
-    "doUpdateDashboardView",
-    "doCompleteSurvey",
-    "doRestartSurvey"
-  );
-  const handleUpdateSurvey = () => {
-    //doUpdateCreateNewSurvey()
-    doUpdateSurvey(survey);
-    doUpdateDashboardView({
-      viewCreateNew: false,
-      viewManage: false,
-      viewActive: true,
-      viewCompleted: false,
-    });
-    if (survey.completed) {
-      doCompleteSurvey(survey);
-    } else {
-      doRestartSurvey(survey); ///i think this creates a bug of adding to the active surveys
-    }
-  };
-  const handleCheckedChange = (field) => (e) => {
-    let val = e.target.checked;
-    console.log(field + " " + val);
-    const s = { ...survey, [field]: val };
-    doUpdateSurvey(s);
-  };
-  return (
-    <div style={{ padding: "50px" }}>
-      <div className="form-group">
-        <div style={{ display: "flex" }}>
-          <label style={{ flexGrow: 1 }}>Survey Name</label>
-        </div>
-        <input
-          type="text"
-          value={survey.name}
-          className="form-control st-input"
-          id="name"
-          placeholder=""
-          readOnly="readonly"
-        />
-      </div>
-      <div className="form-group">
-        <div style={{ display: "flex" }}>
-          <label style={{ flexGrow: 1 }}>Survey Owner</label>
-        </div>
-        <input
-          type="text"
-          value={survey.owner}
-          className="form-control st-input"
-          id="owner"
-          placeholder=""
-          readOnly="readonly"
-        />
-      </div>
-      <div className="form-group">
-        <div style={{ display: "flex" }}>
-          <label style={{ flexGrow: 1 }}>Due Date</label>
-        </div>
-        <input
-          type="date"
-          value={survey.dueDate}
-          className="form-control st-input"
-          id="dueDate"
-          placeholder=""
-          readOnly="readonly"
-        />
-      </div>
-
-      <div>
-        <MemberTable />
-      </div>
-
-      <div style={{ width: "230px" }}>
-        <div div style={{ margin: "15px" }}>
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="surveystatus"
-            onChange={handleCheckedChange("completed")}
-            checked={survey.completed}
-          />
-          <label
-            className="form-check-label"
-            style={{ marginLeft: "5px", marginTop: "2px" }}
-            htmlFor="surveystatus"
-          >
-            Mark Survey Complete
-          </label>
-        </div>
-        <Button
-          size="xl"
-          className="gw-w-full gw-flex gw-items-center gw-justify-center gw-whitespace-nowrap bg-secondary border-r border-white/20 px-4 py-2 first:rounded-l-md last:rounded-r-md st-btn-tb2"
-          onClick={handleUpdateSurvey}
-        >
-          Update Survey
-        </Button>
-        <Button
-          size="xl"
-          className="gw-w-full gw-flex gw-items-center gw-justify-center gw-whitespace-nowrap border-r border-white/20 px-4 py-2 first:rounded-l-md last:rounded-r-md st-btn-tb-delete"
-          onClick={handleUpdateSurvey}
-        >
-          Delete Survey
-        </Button>
-      </div>
-    </div>
-  );
-}
-*/
 import { Button } from "@usace/groundwork";
 import { useConnect } from "redux-bundler-hook";
 import MemberTable from "./member-table";
@@ -168,7 +48,7 @@ export default function ManageSurvey() {
             Manage Survey
           </h1>
           <p className="gw-text-slate-500 font-mono gw-text-xs">
-            ID: {survey.guid}
+            ID: {survey.id}
           </p>
         </div>
         <div
@@ -185,7 +65,7 @@ export default function ManageSurvey() {
       {/* Management Card */}
       <div className="gw-bg-white gw-rounded-lg gw-shadow-md gw-border gw-border-slate-200 gw-overflow-hidden">
         {/* Read-Only Info Header */}
-        <div className="gw-bg-slate-50 gw-p-6 gw-grid gw-grid-cols-1 md:gw-grid-cols-3 gw-gap-6 gw-border-b gw-border-slate-200">
+        <div className="gw-p-8 gw-bg-slate-50 gw-grid gw-grid-cols-1 md:gw-grid-cols-3 gw-gap-6 gw-border-b gw-border-slate-200">
           <div className="gw-flex gw-flex-col">
             <label className="gw-text-[10px] gw-font-bold gw-text-slate-400 gw-uppercase">
               Survey Name
